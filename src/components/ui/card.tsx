5c0f50bcbc08ecf304477c9fc1ed15b92db507ec
github akito-10 <akt.fukuda@gmail.com>
2024-02-05 00:12:20 +0900

    Remove extra space in Card component class name

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -8,7 +8,7 @@ type CardProps = {
 
 export const Card: FC<CardProps> = ({ title, content }) => {
   return (
-    <ShadcnCard className="p-4 w-80 bg-primary ">
+    <ShadcnCard className="p-4 w-80 bg-primary">
       <CardTitle className="text-lg mb-2 font-normal">{title}</CardTitle>
       <CardContent className="p-0 line-clamp-5">{content}</CardContent>
     </ShadcnCard>
