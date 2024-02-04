f3bd33656439a72a1cc42b92c99365eee05a6e16
github akito-10 <akt.fukuda@gmail.com>
2024-02-04 21:32:24 +0900

    Add ChatForm component

--- src/components/domain/chat/chatForm.tsx
+++ src/components/domain/chat/chatForm.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
-import { Button } from "../ui/button";
-import { Textarea } from "../ui/textarea";
+import { Button } from "../../ui/button";
+import { Textarea } from "../../ui/textarea";
 import { Form } from "@remix-run/react";
 
 type FormProps = {
