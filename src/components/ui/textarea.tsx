db193826a6e50065712df2864bb0cff0c88e5a1e
github akito-10 <akt.fukuda@gmail.com>
2024-02-04 21:29:11 +0900

    feat: add placeholder

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -2,20 +2,15 @@ import { ChangeEventHandler, FC } from "react";
 import { Textarea as ShadcnTextarea } from "./shadcn/textarea";
 
 type TextareaProps = {
-  placeholder?: string;
   value: string;
   onChange: ChangeEventHandler<HTMLTextAreaElement>;
 };
 
-export const Textarea: FC<TextareaProps> = ({
-  placeholder,
-  value,
-  onChange,
-}) => {
+export const Textarea: FC<TextareaProps> = ({ value, onChange }) => {
   return (
     <ShadcnTextarea
       className="min-h-fit px-4 py-2 text-base bg-primary text-white overflow-hidden"
-      placeholder={placeholder}
+      placeholder="AI にそうだんする"
       value={value}
       rows={1}
       onChange={onChange}
