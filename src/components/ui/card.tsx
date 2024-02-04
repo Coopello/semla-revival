8698a33d92e4b90070bdc32edfbc1ada6ab17e87
github akito-10 <akt.fukuda@gmail.com>
2024-02-04 13:25:11 +0900

    feat: add custom card

---
+++ src/components/ui/card.tsx
@@ -0,0 +1,16 @@
+import { FC } from "react";
+import { CardContent, CardTitle, Card as ShadcnCard } from "./shadcn/card";
+
+type CardProps = {
+  title: string;
+  content: string;
+};
+
+export const Card: FC<CardProps> = ({ title, content }) => {
+  return (
+    <ShadcnCard className="p-4 w-80">
+      <CardTitle className="text-xl mb-6">{title}</CardTitle>
+      <CardContent className="p-0 line-clamp-5">{content}</CardContent>
+    </ShadcnCard>
+  );
+};
