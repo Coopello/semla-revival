c7f0b337343c65514ae5f4d3542e651a39d1c60f
github akito-10 <akt.fukuda@gmail.com>
2024-02-04 23:50:22 +0900

    Update font style for CardTitle component

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -8,8 +8,8 @@ type CardProps = {
 
 export const Card: FC<CardProps> = ({ title, content }) => {
   return (
-    <ShadcnCard className="p-4 w-80 bg-primary">
-      <CardTitle className="text-lg mb-2">{title}</CardTitle>
+    <ShadcnCard className="p-4 w-80 bg-primary ">
+      <CardTitle className="text-lg mb-2 font-normal">{title}</CardTitle>
       <CardContent className="p-0 line-clamp-5">{content}</CardContent>
     </ShadcnCard>
   );
