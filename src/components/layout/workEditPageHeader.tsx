2cfca4eac38d52ec64f50f47b40b73e644ff92a5
github akito-10 <akt.fukuda@gmail.com>
2024-02-23 21:37:08 +0900

    Refactor WorkEditPageHeader component

--- src/components/layout/workEditPageHeader.tsx
+++ src/components/layout/workEditPageHeader.tsx
@@ -1,20 +1,12 @@
 import { FC } from "react";
 import { Button } from "src/components/ui/button";
 
-type WorkEditPageHeaderProps = {
-  onClickChatButton: () => void;
-};
-
-export const WorkEditPageHeader: FC<WorkEditPageHeaderProps> = ({
-  onClickChatButton,
-}) => {
+export const WorkEditPageHeader: FC = () => {
   return (
     <header className="hidden sm:flex justify-between bg-primary px-4 py-3">
       <Button className="p-0" leftIcon="chevronLeft" />
       <div className="flex gap-4">
-        <Button leftIcon="chat" onClick={onClickChatButton}>
-          AIにそうだん
-        </Button>
+        <Button leftIcon="chat">AIにそうだん</Button>
         <Button leftIcon="book" className="bg-secondary">
           スライドを作成
         </Button>
